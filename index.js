5509db844dcc43eddb98be8fd7d21518ef9db551
Marcin Drozd <marcindrozd@me.com>
2017-07-31 10:38:11 +0200

    Upload files and then remove directory

--- index.js
+++ index.js
@@ -1,51 +1,130 @@
 var AWS = require('aws-sdk');
-var fs = require('fs');
+var fs = require('fs-extra');
 var uuidv4 = require('uuid/v4');
-var childProcess = require("child_process");
-var zipFolder = require('zip-folder');
+var childProcess = require('child_process');
+var archiver = require('archiver');
+var async = require('async');
 
 // Creating AWS client
 var s3 = new AWS.S3();
 
 exports.handler = (event, context, callback) => {
-  var saveFolder = uuidv4()
-  fs.mkdir('./tmp/' + saveFolder, function(error) {
-    if (error) throw error;
+  var saveFolder = uuidv4();
+  var filePath = "./tmp/" + saveFolder;
+
+  function makeDirectory(callback) {
+    fs.mkdir('./tmp/' + saveFolder, function(error) {
+      if (error) throw error;
+      callback(null, saveFolder);
+    })
+  }
+
+  function getVideo(saveFolder, callback) {
     s3.getObject({
       Bucket: 'qkvideo-dev-videos',
-      Key: '1011/asset-92e3ed51-71e4-4a0a-826d-e488b4f16449/video.mp4'
+      Key: '2820/asset-b216d618-3ac0-4131-8292-3056dd1c1a7f/video.mp4'
     }, function(error, data) {
-      if (error !== null) {
-        console.log('Failure:', error)
-      } else {
-        var filePath = "./tmp/" + saveFolder
+      if (error) throw error;
+      callback(null, saveFolder, data)
+    });
+  }
 
-        console.log("Loaded " + data.ContentLength + " bytes");
-        fs.writeFile(filePath + '/video.mp4', data.Body, function(error) {
-          if (error) throw error;
-          console.log('File saved!');
+  function saveFile(saveFolder, data, callback) {
+    fs.writeFile(filePath + '/video.mp4', data.Body, function(error) {
+      if (error) throw error;
+      callback()
+    });
+  }
 
-          var framesToExtract = event.framesToExtract.map(
-            function(frame) {
-              timeframe = parseInt(frame).toString()
-              return timeframe.length === 1 ? "0" + timeframe : timeframe
-            }
-          );
-
-          framesToExtract.forEach(function(frame) {
-            childProcess.exec("ffmpeg -i " + filePath + "/video.mp4 -ss 00:00:" + frame + " -vframes 1 -f image2 '" + filePath + "/image" + Date.now() + ".jpg'",
-              function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                  console.log('exec error: ' + error);
-                }
-              });
-          });
-        });
+  function prepareFramesToExtract(callback) {
+    var framesToExtract = event.framesToExtract.map(
+      function(frame) {
+        timeframe = parseInt(frame).toString()
+        return timeframe.length === 1 ? "0" + timeframe : timeframe
       }
+    );
+    callback(null, framesToExtract)
+  }
+
+  function extractFrames(framesToExtract, callback) {
+    framesToExtract.forEach(function(frame) {
+      childProcess.execSync("ffmpeg -i " + filePath + "/video.mp4 -ss 00:00:" + frame + " -vframes 1 -f image2 '" + filePath + "/image" + Date.now() + ".jpg'");
+    });
+
+    callback();
+  }
+
+  function zipFiles(callback) {
+    var output = fs.createWriteStream(filePath + '/video.zip');
+    var archive = archiver('zip');
+
+    output.on('close', function() {
+      console.log(archive.pointer() + ' total bytes');
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+      callback(null);
+    });
+
+    archive.on('error', function(err) {
+      throw err;
+    });
+
+    archive.pipe(output);
+
+    fs.readdirSync(filePath).forEach(file => {
+      if (file === 'video.zip') { return }
+      console.log('Archiving', file);
+      archive.append(fs.createReadStream(filePath + '/' + file), { name: file })
+    });
+
+    archive.finalize()
+  }
+
+  function uploadFile(callback) {
+    console.log('Uploading...', filePath + '/video.zip')
+    var readStream = fs.createReadStream(filePath + '/video.zip');
+
+    s3.upload({
+      Bucket: 'qkvideo-dev-videos',
+      Key: '2820/asset-b216d618-3ac0-4131-8292-3056dd1c1a7f/video.zip',
+      ContentType: 'application/zip',
+      Body: readStream,
     })
-  })
+    .send(function (error, result) {
+      readStream.close();
+      if (error) { throw error }
+      console.log('upload completed.');
+      callback(null, result);
+    });
+  }
+
+  function pingEndpointWithUrl(response, callback) {
+    console.log('File url:', response.Location)
+
+    callback(null, 'All done')
+  }
+
+
+  function check(arg, callback) {
+    console.log('Now in check!', arg);
+    callback();
+  }
+
+  async.waterfall([
+    makeDirectory,
+    getVideo,
+    saveFile,
+    prepareFramesToExtract,
+    extractFrames,
+    zipFiles,
+    uploadFile,
+    pingEndpointWithUrl,
+    check
+  ], function(error, result) {
+    if (error) { throw error }
+    console.log('Removing directory')
+    fs.removeSync(filePath)
+    console.log('All done!');
+  });
 
   context.succeed("Yay :)");
 }
